0bacc45d67b351727630b5faa9fc38e1bc3d0f65
sanyi24 <nagysanyi96@gmail.com>
2022-09-09 10:37:19 +0200

    Comments added

--- main.cpp
+++ main.cpp
@@ -2,23 +2,22 @@
 
 constexpr int N_ELEMENTS = 100;
 
-int main()
-{
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
-    {
+int main () {
+    // Firstly init array, the double each value?
+    // Separate functionality!
+    int* b = new int[NELEMENTS];            // NELEMENTS -> N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa'  // Missing ; and std::endl;
+    for (int i = 0;)                        // Fix: loop, logic inside the loop
+    {  
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
-    {
-        std::cout << "Ertek:"
-    }    
+    for (int i = 0; i; i++) {               // Iterate through the elements of b
+        std::cout << "Ertek:"               // Print out value
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
-    {
-        atlag += b[i]
+    for (int i = 0; i < N_ELEMENTS, i++) {  // Use ;
+        atlag += b[i]                       // Missing ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
